ae999d944e56f0467a6732cf7102c532d74a94c6
valseLee <sollleky72@gmail.com>
2022-08-06 12:47:30 +0900

    update: 테이블뷰, 테이블뷰셀 설정

--- chatting/chatting/Controllers/ConversationsViewController.swift
+++ chatting/chatting/Controllers/ConversationsViewController.swift
@@ -7,19 +7,22 @@
 
 import UIKit
 
-class ConversationsViewController: UIViewController {
+final class ConversationsViewController: UIViewController {
+	private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 		configViewUI()
-		configNavUI()
+		configNavBarUI()
+		configTableView()
     }
 	
 	func configViewUI() {
 		view.backgroundColor = .white
+		view.addSubview(tableView)
 	}
 	
-	func configNavUI() {
+	func configNavBarUI() {
 		let image = UIImage(systemName: "person.circle.fill")
 		navigationItem.leftBarButtonItem = UIBarButtonItem(
 			image: image,
@@ -45,7 +48,38 @@ class ConversationsViewController: UIViewController {
 		navigationItem.title = "Messages"
 	}
 	
+	func configTableView() {
+		print(#function)
+		tableView.backgroundColor = .white
+		tableView.rowHeight = 80
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: Cell.cellReuse)
+	
+		tableView.frame = view.frame
+		tableView.tableFooterView = UIView()
+		
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
+	
 	@objc func showProfile() {
-		print("?/")
+		print("?")
+	}
+}
+
+extension ConversationsViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(indexPath.row)
+	}
+}
+
+extension ConversationsViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: Cell.cellReuse, for: indexPath)
+		cell.textLabel?.text = "?"
+		return cell
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 2
 	}
 }
---
+++ chatting/chatting/Models/Constanats.swift
@@ -0,0 +1,12 @@
+//
+//  Constanats.swift
+//  chatting
+//
+//  Created by 이승준 on 2022/08/06.
+//
+
+import UIKit
+
+public struct Cell {
+	static let cellReuse = "ConversationCell"
+}
