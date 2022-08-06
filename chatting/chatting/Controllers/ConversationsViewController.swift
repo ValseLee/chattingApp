b776770f3441f901fe84143d00e0e847c5f3f128
valseLee <sollleky72@gmail.com>
2022-08-06 12:22:03 +0900

    init: 채팅앱으로 채팅 구현

---
+++ chatting/chatting/Controllers/ConversationsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConversationsViewController.swift
+//  chatting
+//
+//  Created by 이승준 on 2022/08/06.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		view.backgroundColor = .red
+    }
+	
+}
